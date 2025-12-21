300a30320c2f65a38e702c1ac6c2a157c4c8f4c9
Yassine Moudarrir <moudarrir@192.168.1.6>
2025-12-21 15:07:51 -0800

    Fix Post Now to immediately publish instead of just scheduling

--- src/Http/Controllers/SchedulePostController.php
+++ src/Http/Controllers/SchedulePostController.php
@@ -4,6 +4,7 @@ namespace Inovector\Mixpost\Http\Controllers;
 
 use Illuminate\Http\JsonResponse;
 use Illuminate\Routing\Controller;
+use Inovector\Mixpost\Actions\PublishPost;
 use Inovector\Mixpost\Facades\Settings;
 use Inovector\Mixpost\Http\Requests\SchedulePost;
 use Inovector\Mixpost\Util;
@@ -13,6 +14,13 @@ class SchedulePostController extends Controller
     public function __invoke(SchedulePost $schedulePost): JsonResponse
     {
         $schedulePost->handle();
+
+        // If postNow is true, immediately publish instead of waiting for cron
+        if ($schedulePost->input('postNow')) {
+            (new PublishPost())($schedulePost->post);
+            
+            return response()->json("Your post is being published now!");
+        }
 
         $scheduledAt = $schedulePost->getDateTime()->tz(Settings::get('timezone'))->format("D, M j, " . Util::timeFormat());
 
