a87d08e70db33b3dfb5c8320543d6e7eef7b0f4e
Yassine Moudarrir <moudarrir@Yassines-MacBook-Pro.local>
2025-12-21 23:00:07 -0800

    Fix first comment not saving - add validation rule

--- src/Http/Requests/PostFormRequest.php
+++ src/Http/Requests/PostFormRequest.php
@@ -16,6 +16,7 @@ class PostFormRequest extends FormRequest
             'date' => ['nullable', 'date', 'date_format:Y-m-d'],
             'time' => ['nullable', 'date_format:H:i'],
             'versions' => ['required', 'array', 'min:1'],
+            'versions.*.first_comment' => ['nullable', 'string', 'max:2000'],
             'versions.*.content.*.body' => ['nullable', 'string', 'max:5000'],
             'versions.*.content.*.media' => ['array'],
             'versions.*.content.*.media.*' => ['integer']
